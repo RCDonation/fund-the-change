cb0a76d84b35825ea29079b685bea034c8484abe
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-11-30 16:54:45 -0500

    28th deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-
+const path = require('path')
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000
@@ -53,6 +53,10 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static("client/build"))
+  app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
 }
 
 app.listen(PORT, host, console.log(`server is running on ${PORT}`));
